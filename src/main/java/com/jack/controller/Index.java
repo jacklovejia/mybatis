a4225e4b9e34da8ec834025db34c54f05e222fcb
Jack <1109546392@qq.com>
2019-12-09 22:14:06 +0800

    测试图片

---
+++ src/main/java/com/jack/controller/Index.java
@@ -0,0 +1,13 @@
+package com.jack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "thymeleaf/index";
+    }
+}
